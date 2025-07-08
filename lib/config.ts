a51fd127c9fef4748eea1b47797941c92602167f
Dmytro <dimboychuk1995@gmail.com>
2025-07-08 12:06:58 -0500

    works from iphone

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const API_URL =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://192.168.0.229:5000"
-    : "https://9bce-73-72-97-139.ngrok-free.app";
+    : "https://38f9244c3e57.ngrok-free.app";
